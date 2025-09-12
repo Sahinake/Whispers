7d296ed41fa217f792bbcf72abc5f161869c8dc5
Sahinake <lumaluiza1992@gmail.com>
2025-09-12 12:55:11 -0300

    feat(menu): add some sound effects and change background image

--- whispers/scenes/scripts/initial_menu.gd
+++ whispers/scenes/scripts/initial_menu.gd
@@ -1,34 +1,67 @@
 extends Control
 
+@onready var music = $MusicMenuSound
+@onready var water = $WaterMenuSound
+@onready var effect_player = $Effects/EffectsSound
+
+# Sons aleatórios (sussurros, animais marinhos, etc.)
+@export var random_sounds : Array[AudioStream] = []
+
+# Volumes (em decibéis)
+@export var music_volume_db := 1.0  
+@export var water_volume_db := 3.0   
+@export var effects_volume_db := - 0.5
+
 func _ready():
+	# conecta botões
 	var newGameBtn = $VBoxContainer/NewGameButton
 	if newGameBtn:
 		newGameBtn.pressed.connect(_on_novo_jogo_pressed)
-		print("Conectado com sucesso!")
-	else:
-		print("Botão não encontrado!")
-	
+
 	var loadGameButton = $VBoxContainer/LoadGameButton
 	if loadGameButton:
 		loadGameButton.pressed.connect(_on_carregar_jogo_pressed)
-		print("Conectado com sucesso!")
-	else:
-		print("Botão não encontrado!")
-		
+
 	var settingsButton = $VBoxContainer/Settings
 	if settingsButton:
 		settingsButton.pressed.connect(_on_configuracoes_pressed)
-		print("Conectado com sucesso!")
-	else:
-		print("Botão não encontrado!")
-		
+
 	var exitButton = $VBoxContainer/Exit
 	if exitButton:
 		exitButton.pressed.connect(_on_sair_pressed)
-		print("Conectado com sucesso!")
-	else:
-		print("Botão não encontrado!")
-		
+
+	# aplica volumes
+	music.volume_db = music_volume_db
+	water.volume_db = water_volume_db
+	effect_player.volume_db = effects_volume_db
+
+	# inicia música e água em loop
+	if music.stream:
+		music.stream.loop = true
+		music.play()
+
+	if water.stream:
+		water.stream.loop = true
+		water.play()
+
+	# inicia ciclo de sons aleatórios
+	play_random_effect()
+
+func play_random_effect():
+	if random_sounds.is_empty():
+		return
+
+	# escolhe som aleatório
+	var sound = random_sounds[randi() % random_sounds.size()]
+	effect_player.stream = sound
+	effect_player.play()
+
+	# agenda próximo efeito em intervalo randômico
+	var wait_time = randf_range(5.0, 15.0) # ajusta intervalo aqui
+	await get_tree().create_timer(wait_time).timeout
+	play_random_effect()
+
+# ==== Botões do menu ====
 func _on_novo_jogo_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 
